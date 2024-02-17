123d5400fff707a821fa4b9798572d03d69aff31
mohamad akar <MoeAkar123456@gmail.com>
2024-02-17 00:30:13 -0800

    creating header comp

--- clientSide/src/App.jsx
+++ clientSide/src/App.jsx
@@ -4,20 +4,22 @@ import Signin from "./pages/Signin"
 import Profile from "./pages/Profile"
 import About from "./pages/About"
 import Signup from "./pages/Signup"
+import Header from "./components/Header"
 
 export default function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/sign-in" element={<Signin />} />
-    <Route path="/sign-up" element={<Signup />} />
-    <Route path="/profile" element={<Profile />} />
-    <Route path="/about" element={<About />} />
-  
-  
-  
-  
-  </Routes>
+    return <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/about" element={<About />} />
+    
+    
+    
+    
+    </Routes>
 
-</BrowserRouter>
+  </BrowserRouter>
 }
