766bbdab98082ef3ad63cc30a8de76d88ca02aae
mohamad akar <MoeAkar123456@gmail.com>
2024-02-18 22:12:25 -0800

    crate signin

--- API/controllers/authController.js
+++ API/controllers/authController.js
@@ -1,6 +1,7 @@
 const {User}=require("../models/User");
 const bcrypt=require("bcryptjs")
 const asyncHandler=require("express-async-handler")
+const jwt =require("jsonwebtoken")
 
 module.exports.signUp=asyncHandler(async(req,res)=>{
     const {username,email,password} =req.body
@@ -10,4 +11,20 @@ module.exports.signUp=asyncHandler(async(req,res)=>{
     res.status(201).json({message:"user created successfully"})
     res.status(500).json(error.message)
     
+})
+
+module.exports.signIn=asyncHandler(async(req,res)=>{
+    const {email,password}=req.body;
+    const validUser=await User.findOne({email});
+    if(!validUser){
+        return res.status(404).json({message:"Not Found"})
+    }
+    const validPassword=bcrypt.compareSync(password,validUser.password);
+    if(!validPassword){
+        return res.status(400).json({message:"Wrong credentials"});
+    }
+    const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+    const {password:pass,...rest}=validUser._doc;
+    res.cookie('access_token',token,{httpOnly:true,expires:new Date(Date.now()+24*60*60*1000)})
+    .status(200).json(rest);
 })
--- API/routes/authRoute.js
+++ API/routes/authRoute.js
@@ -1,7 +1,8 @@
-const { signUp } = require("../controllers/authController");
+const { signUp, signIn } = require("../controllers/authController");
 const router=require("express").Router();
 
 router.post("/signup",signUp)
+router.post("/signin",signIn)
 
 
 module.exports=router;
